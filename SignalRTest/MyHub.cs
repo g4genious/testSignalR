3e4059feba6cd0f61d81badc6cdc5536bb39bfc2
SALAHUDIN` <SALAHUDIN`@DESKTOP-FLRFM6B>
2017-08-19 18:23:09 +0500

    Add project files.

---
+++ SignalRTest/MyHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRTest
+{
+    public class MyHub:Hub
+    {
+        public void Chat(string name,string Message)
+        {
+            Clients.All.SendChat(name, Message);
+        }
+    }
+}
---
+++ SignalRTest/MySignalRHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SignalRTest
+{
+    public class MySignalRHub:Hub
+    {
+        public void SendMessage(string name,string Message)
+        {
+            Clients.All.ShowMessageClients(name,Message);
+        }
+    }
+}
